ee970de04c2cf2aad9870420d6ace957edbc1862
Jose Barrantes <jose.barrantes@hotmail.es>
2022-02-10 13:18:30 -0500

    Added providers file on root dir.

--- main.tf
+++ main.tf
@@ -8,11 +8,6 @@ terraform {
   required_version = ">= 0.14.9"
 }
 
-provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
-}
-
 variable "bucket_name" {
   type    = string
   default = "travisci-bucket-jose-barrantes"
