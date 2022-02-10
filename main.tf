59801264ddf7394eb2baf88b1affc5eb1c8aea78
Jose Barrantes <jose.barrantes@hotmail.es>
2022-02-10 14:11:48 -0500

    Deleted unnecessary code.

--- main.tf
+++ main.tf
@@ -1,17 +1,5 @@
-variable "bucket_name" {
-  type    = string
-  default = "travisci-bucket-jose-barrantes"
-}
-
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name
-  tags = {
-    Name = var.bucket_name
-  }
-}
-
 module "s3_bucket_test" {
-  source = "../modules/s3_test"
-    environment = var.environment
-    bucket_name = var.bucket_name
+  source      = "../modules/s3_test"
+  environment = var.environment
+  bucket_name = var.bucket_name
 }
