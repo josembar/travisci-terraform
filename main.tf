5de33663b9d4119e9dbeadda8f57e53d4c47f8bf
Jose Barrantes <jose.barrantes@hotmail.es>
2022-02-21 20:36:27 -0500

    Deleted unnecessary code.

--- main.tf
+++ main.tf
@@ -3,9 +3,3 @@ module "s3_bucket_test" {
   environment = "${terraform.workspace}"
   bucket_name = var.bucket_name
 }
-
-#resource "aws_s3_bucket" "bucket_two" {
-#  tags = {
-#    Environment = var.environment
-#  }
-#}
